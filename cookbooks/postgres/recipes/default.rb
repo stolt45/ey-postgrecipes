0744063c1afdf562f3cd3d2437925c41a7671f21
Christopher Stolt <stolt45@christopher-stolts-macbook-pro.local>
2009-06-24 23:18:14 -0600

    adding in a sym link

--- cookbooks/postgres/recipes/default.rb
+++ cookbooks/postgres/recipes/default.rb
@@ -59,7 +59,7 @@ node[:applications].each do |app_name,data|
   end
 
   execute "custom-symlinks" do
-    command "ln -nfs /data/#{app_name}/current/config/database.yml /data/#{app_name}/shared/config/database.yml.psql
+    command "ln -nfs /data/#{app_name}/current/config/database.yml /data/#{app_name}/shared/config/database.yml.psql"
     action :run
     user #{user[:username]}
   end
